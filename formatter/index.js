4f81ae75d1369a445b6ba14a618147192a980014
Adam Fields <adam.elliot.fields@gmail.com>
2017-09-29 11:43:27 -0400

    Fixed formatter.

--- formatter/index.js
+++ formatter/index.js
@@ -1,39 +1,41 @@
 /**
- * Copyright (c) 2015-present, Facebook, Inc.
+ * Adapted from https://github.com/facebookincubator/create-react-app/tree/master/packages/react-dev-utils
  *
- * This source code is licensed under the MIT license found.
- * 
+ * Original copyright (c) 2015-present, Facebook, Inc, licensed under the MIT license.
+ *
  * https://github.com/facebookincubator/create-react-app/blob/master/LICENSE
  */
 
-'use strict';
-
 const chalk = require('chalk');
+const stripAnsi = require('strip-ansi');
 const table = require('text-table');
 
-function isError (message) {
-  if (message.fatal || message.severity === 2) {
-    return true;
-  }
-  return false;
-}
+const isError = (message) => {
+  // message.fatal is a boolean
+  return message.fatal || message.severity === 2;
+};
 
-function formatter (results) {
+const formatter = (results) => {
   let output = '\n';
   let hasErrors = false;
   let reportContainsErrorRuleIDs = false;
 
-  results.forEach(result => {
+  results.forEach((result) => {
     let messages = result.messages;
+
     if (messages.length === 0) {
       return;
     }
 
-    messages = messages.map(message => {
+    messages = messages.map((message) => {
       let messageType;
+      let line = message.line || 0;
+      let position = chalk.cyan(`Line ${line}:`);
+
       if (isError(message)) {
         messageType = 'error';
         hasErrors = true;
+
         if (message.ruleId) {
           reportContainsErrorRuleIDs = true;
         }
@@ -41,32 +43,31 @@ function formatter (results) {
         messageType = 'warn';
       }
 
-      let line = message.line || 0;
-      let position = chalk.bold('Line ' + line + ':');
       return [
         '',
         position,
         messageType,
         message.message.replace(/\.$/, ''),
-        chalk.underline(message.ruleId || '')
+        chalk.red(message.ruleId || '')
       ];
     });
 
     // if there are error messages, we want to show only errors
     if (hasErrors) {
-      messages = messages.filter(m => m[2] === 'error');
+      messages = messages.filter((m) => m[2] === 'error');
     }
 
     // add color to rule keywords
-    messages.forEach(m => {
+    messages.forEach((m) => {
       m[4] = m[2] === 'error' ? chalk.red(m[4]) : chalk.yellow(m[4]);
       m.splice(2, 1);
     });
 
     let outputTable = table(messages, {
       align: ['l', 'l', 'l'],
+
       stringLength (str) {
-        return chalk.stripColor(str).length;
+        return stripAnsi(str).length;
       }
     });
 
@@ -74,20 +75,10 @@ function formatter (results) {
   });
 
   if (reportContainsErrorRuleIDs) {
-    // Unlike with warnings, we have to do it here.
-    // We have similar code in react-scripts for warnings,
-    // but warnings can appear in multiple files so we only
-    // print it once at the end. For errors, however, we print
-    // it here because we always show at most one error, and
-    // we can only be sure it's an ESLint error before exiting
-    // this function.
-    output +=
-      'Search for the ' +
-      chalk.underline(chalk.red('keywords')) +
-      ' to learn more about each error.';
+    output += `Search for the ${chalk.yellow('keywords')} to learn more about each error.\nTo ignore, add ${chalk.gray('// eslint-disable-next-line')} to the line before.`;
   }
 
   return output;
-}
+};
 
 module.exports = formatter;
